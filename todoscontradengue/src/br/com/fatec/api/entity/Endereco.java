65905090e652ead87076589f933f23cf64b56094
João Carlos Batista <joao_nad@hotmail.com>
2016-03-01 19:41:50 -0300

    Teste 3

--- todoscontradengue/src/br/com/fatec/api/entity/Endereco.java
+++ todoscontradengue/src/br/com/fatec/api/entity/Endereco.java
@@ -3,6 +3,42 @@ package br.com.fatec.api.entity;
 public class Endereco {
 	
 	private String rua;
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getEstado() {
+		return estado;
+	}
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	public String getEstado1() {
+		return estado1;
+	}
+	public void setEstado1(String estado1) {
+		this.estado1 = estado1;
+	}
+	public String getA() {
+		return a;
+	}
+	public void setA(String a) {
+		this.a = a;
+	}
 	private String cep;
 	private String cidade;
 	private String estado;
